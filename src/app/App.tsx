9139e27db51f20a25efbbd0088acc6f9b74cb54b
Ezakamak <116961300+Ezakamak@users.noreply.github.com>
2026-05-11 21:18:04 +0300

    Mount scroll progress bar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,6 +12,7 @@ import { ReactionTime } from "./components/ReactionTime";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 import { Loader } from "./components/Loader";
+import { ScrollProgress } from "./components/ScrollProgress";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -41,6 +42,7 @@ export default function App() {
   return (
     <LanguageProvider>
       <Loader show={loading || languageLoading} />
+      <ScrollProgress />
       <div className="min-h-screen bg-black dark">
         <Navbar />
         <Hero />
