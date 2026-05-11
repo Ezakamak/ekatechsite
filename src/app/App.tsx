1cfdcdbe1eedf6f7ef885d4d060554c9467023c0
Ezakamak <116961300+Ezakamak@users.noreply.github.com>
2026-05-11 19:50:20 +0300

    Show slider on homepage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
 import { About } from "./components/About";
 import { Projects } from "./components/Projects";
+import { BeforeAfter } from "./components/BeforeAfter";
 import { TechStack } from "./components/TechStack";
 import { CTA } from "./components/CTA";
 import { ReactionTime } from "./components/ReactionTime";
@@ -12,24 +13,19 @@ import { Loader } from "./components/Loader";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
-
   useEffect(() => {
-    const timer = window.setTimeout(() => {
-      setLoading(false);
-    }, 3600);
-
+    const timer = window.setTimeout(() => setLoading(false), 3600);
     return () => window.clearTimeout(timer);
   }, []);
-
   return (
     <>
       <Loader show={loading} />
-
       <div className="min-h-screen bg-black dark">
         <Navbar />
         <Hero />
         <About />
         <Projects />
+        <BeforeAfter />
         <TechStack />
         <CTA />
         <ReactionTime />
