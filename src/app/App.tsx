350b8bc4f2e11cf50eb477abb833cc1d63ed5f3a
Ezakamak <116961300+Ezakamak@users.noreply.github.com>
2026-05-14 17:37:00 +0300

    Use Core Clash visual effects wrapper

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -36,7 +36,7 @@ import { AdminProjectTools } from "./components/AdminProjectTools";
 import { MaintenancePanel } from "./components/MaintenancePanel";
 import { MaintenanceGate } from "./components/MaintenanceGate";
 import { OffPage } from "./components/OffPage";
-import { CoreClash } from "./components/CoreClashSequential";
+import { CoreClash } from "./components/CoreClashVisualEffects";
 
 function getCurrentPath() {
   if (typeof window === "undefined") return "/";
